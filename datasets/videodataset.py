7a8a9e60326f461eec0bd29c7c9d408538f75204
lhlihua <73414619+lhlihua@users.noreply.github.com>
2023-05-05 17:31:42 +0800

    Add files via upload

---
+++ datasets/videodataset.py
@@ -0,0 +1,142 @@
+import json
+from pathlib import Path
+
+import torch
+import torch.utils.data as data
+
+from .loader import VideoLoader
+
+
+def get_class_labels(data):
+    class_labels_map = {}
+    index = 0
+    for class_label in data['labels']:
+        class_labels_map[class_label] = index
+        index += 1
+    return class_labels_map
+
+
+def get_database(data, subset, root_path, video_path_formatter):
+    video_ids = []
+    video_paths = []
+    annotations = []
+
+    for key, value in data['database'].items():
+        this_subset = value['subset']
+        if this_subset == subset:
+            video_ids.append(key)
+            annotations.append(value['annotations'])
+            if 'video_path' in value:
+                video_paths.append(Path(value['video_path']))
+            else:
+                label = value['annotations']['label']
+                video_paths.append(video_path_formatter(root_path, label, key))
+
+    return video_ids, video_paths, annotations
+
+
+class VideoDataset(data.Dataset):
+
+    def __init__(self,
+                 root_path,
+                 annotation_path,
+                 subset,
+                 spatial_transform=None,
+                 temporal_transform=None,
+                 target_transform=None,
+                 video_loader=None,
+                 video_path_formatter=(lambda root_path, label, video_id:
+                                       root_path / label / video_id),
+                 image_name_formatter=lambda x: f'{x:05d}.jpg',
+                 target_type='label'):
+        self.data, self.class_names = self.__make_dataset(
+            root_path, annotation_path, subset, video_path_formatter)
+
+        self.spatial_transform = spatial_transform
+        self.temporal_transform = temporal_transform
+        self.target_transform = target_transform
+
+        if video_loader is None:
+            self.loader = VideoLoader(image_name_formatter)
+        else:
+            self.loader = video_loader
+
+        self.target_type = target_type
+    # 通过json文件生成dataset video_ids: video的名称例如 v_ApplyEyeMakeup_g03_c02 video_paths 视频路径
+    # annotations 0000:{'label': 'ApplyEyeMakeup', 'segment': [1, 134]}
+    def __make_dataset(self, root_path, annotation_path, subset,
+                       video_path_formatter):
+        with annotation_path.open('r') as f:
+            data = json.load(f)
+        video_ids, video_paths, annotations = get_database(
+            data, subset, root_path, video_path_formatter)
+        class_to_idx = get_class_labels(data)
+        idx_to_class = {}
+        for name, label in class_to_idx.items():
+            idx_to_class[label] = name
+
+        n_videos = len(video_ids)
+        dataset = []
+        # 将视频集合根据json 分成一个个的sample(一个视频对应一个sample)
+        for i in range(n_videos):
+            if i % (n_videos // 5) == 0:
+                print('dataset loading [{}/{}]'.format(i, len(video_ids)))
+
+            if 'label' in annotations[i]:
+                label = annotations[i]['label']
+                label_id = class_to_idx[label]
+            else:
+                label = 'test'
+                label_id = -1
+
+            video_path = video_paths[i]
+            if not video_path.exists():
+                continue
+
+            segment = annotations[i]['segment']
+            if segment[1] == 1:
+                continue
+
+            frame_indices = list(range(segment[0], segment[1]))
+            sample = {
+                'video': video_path,
+                'segment': segment,
+                'frame_indices': frame_indices,
+                'video_id': video_ids[i],
+                'label': label_id
+            }
+            dataset.append(sample)
+
+        return dataset, idx_to_class
+    # 加载图片 对其进行spatial_transform compose列表中的转换 并把其转化为tensor形式的(有进行归一化和标准化)
+    def __loading(self, path, frame_indices):
+        clip = self.loader(path, frame_indices)
+        if self.spatial_transform is not None:
+            self.spatial_transform.randomize_parameters()
+            clip = [self.spatial_transform(img) for img in clip]
+        # 扩展维度把时间维度加入 把多帧图片品在一起
+        clip = torch.stack(clip, 0).permute(1, 0, 2, 3)
+
+        return clip
+    # 获取一个视频中的一个batch_size的视频帧 通过temporal_transform compose列表中的转换 同时会调用__loading对这些视频帧进行转换
+    # 返回的是转换后的视频帧 和 target
+    def __getitem__(self, index):
+        path = self.data[index]['video']
+        if isinstance(self.target_type, list):
+            target = [self.data[index][t] for t in self.target_type]
+        else:
+            target = self.data[index][self.target_type]
+
+        frame_indices = self.data[index]['frame_indices']
+        if self.temporal_transform is not None:
+            frame_indices = self.temporal_transform(frame_indices)
+        # 对获取到的视频帧进行处理
+        clip = self.__loading(path, frame_indices)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return clip, target
+
+    def __len__(self):
+        return len(self.data)
